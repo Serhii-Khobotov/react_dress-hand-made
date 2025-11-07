f70dba7cea48bcd808baca8305e321dc905722bc
Serhii-Khobotov <hobbitura@gmail.com>
2025-11-07 15:41:47 +0200

    initial commit

--- src/features/products/productsData.ts
+++ src/features/products/productsData.ts
@@ -3,14 +3,14 @@ export const productsData = [
     id: 1,
     name: 'В’язаний кардиган “Затишок”',
     price: 2400,
-    image: '/img/cardigan.jpeg',
+    image: './img/cardigan.jpeg',
     description: 'Кардиган ручної роботи з вовни. Теплий, легкий і стильний.'
   },
   {
     id: 2,
     name: 'Светр “Зимова казка”',
     price: 1900,
-    image: '/img/sweater.jpeg',
+    image: './img/sweater.jpeg',
     description: 'В’язаний светр із натуральної пряжі. Ідеальний для холодної пори.'
   },
 ];
